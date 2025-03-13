50100bf3ac4d03a56bfa119706f19cb96dc1b521
adcockabundis486 <adcockabundis486@gmail.com>
2025-03-13 18:16:39 +0000

    Add 9.js

---
+++ src/9.js
@@ -0,0 +1,10 @@
+var express = require('express');
+var app = express();
+
+app.get('/', function(req, res) {
+  // Your math homework code goes here!
+});
+
+app.listen(3000, function() {
+  console.log('Math homework server started on port 3000');
+});
