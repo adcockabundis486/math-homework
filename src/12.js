05150d2ef5b68fcef332268b394871de296bb8ff
adcockabundis486 <adcockabundis486@gmail.com>
2025-03-16 17:47:44 +0000

    Add 12.js

---
+++ src/12.js
@@ -0,0 +1,14 @@
+
+const mathProblems = {
+  "problem1": {
+    "question": "What is the value of x in the equation 2x + 5 = 11?",
+    "answer": "3"
+  },
+  "problem2": {
+    "question": "Find the value of y in the equation y = mx + b, where m = 2 and b = -3.",
+    "answer": "-1"
+  }
+}
+
+console.log(mathProblems["problem1"].question) // Output: What is the value of x in the equation 2x + 5 = 11?
+console.log(mathProblems["problem1"].answer) // Output: 3
